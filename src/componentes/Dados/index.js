532d9e9131aa2de3b7cca582a3df2b3f850ceb61
felipe44y <Felipelipim44@gmail.com>
2021-09-29 22:32:29 -0300

    projeto concluido

--- src/componentes/Dados/index.js
+++ src/componentes/Dados/index.js
@@ -6,7 +6,7 @@ export default function Dados(){
     return(
         <div className="fora">
             <h1 id="gg">Sobre mim</h1>
-            <h1 className="ff">Meu nome é <span>{"<Felipe/>"}</span></h1>
+            <h1 className="ff">Meu nome é <span style={{color:"#8A2BE2"}}>{"<Felipe/>"}</span></h1>
             <p className="about"><strong>Tenho 20 anos, comecei com front end em 2021, gosto muito dessa área e estou  me dedicando para sempre procura aprender novas linguagens, aproveitando cada etapa do processo de design. Sempre farei o maximo para entregar um exelente serviço.</strong></p>
             <div className="contato">
                 <p><strong>Brazil</strong></p>
