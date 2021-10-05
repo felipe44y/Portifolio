2e8bde629010170509d90908f3b7d75b22cfe270
felipe44y <Felipelipim44@gmail.com>
2021-10-05 14:39:55 -0300

    projeto completo

--- src/componentes/Header/Index.js
+++ src/componentes/Header/Index.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import Particles from 'react-tsparticles'
 import './style.css'
 
 export default function Header(){
  
     return(
-            <div className="perfil">
-                <div id="stars1"></div>
-                <div id="stars2"></div>
-                <div id="stars3"></div>
-                <div className="nomes">
-                    <h1>Hello, welcome to my personal website, my name is <span id="Nome">{"<Felipe/>"}</span></h1>
-                    <p>Engenheiro de Software</p>
-                </div> 
-                <section className="foto">
-                    <img alt ="imgagem"src={'./perfil.svg'}/> 
-                </section>  
-            </div>
+        <div className="perfil">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div className="nomes">
+                <h1>Hello, welcome to my personal website, my name is <span id="Nome">{"<Felipe/>"}</span></h1>
+                <p>Engenheiro de Software</p>
+            </div> 
+            <section className="foto">
+                <img alt ="imgagem"src={'./perfil.svg'}/> 
+            </section> 
+        </div>
     )
 }
